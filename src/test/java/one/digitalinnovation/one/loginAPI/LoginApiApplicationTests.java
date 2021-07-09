4eb8703d784ce72c064d3e16bd34f4dd032920f5
danielnatham <danielnatham@gmail.com>
2021-07-09 08:44:21 -0300

    first commit

---
+++ src/test/java/one/digitalinnovation/one/loginAPI/LoginApiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.one.loginAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
